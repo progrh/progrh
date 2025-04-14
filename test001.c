efd9e6d892aa5e34da9f607fd06c6e576840b94b
Ramiz Hassan Protik <116564231+progrh@users.noreply.github.com>
2025-04-15 00:37:28 +0600

    Create test001.c
    my first c code

---
+++ test001.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+  int a = 5;
+  float b = 5.5;
+  double c = 550.55;
+  char d = 'A';
+  string e = 'It is a String';
+  
+    printf("%d \n",a);
+    printf("%f \n",b);
+    printf("%f \n",c);
+    printf("%s \n",d);
+    printf("%c \n",e);
+  return 0;
+  
+}
