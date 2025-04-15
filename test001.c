7b93f7781e99cf4deae88b2942cc595124de4c41
Ramiz Hassan Protik <116564231+progrh@users.noreply.github.com>
2025-04-15 19:11:03 +0600

    Update test001.c
    Improved the string variable

--- test001.c
+++ test001.c
@@ -4,13 +4,13 @@ int main(){
   float b = 5.5;
   double c = 550.55;
   char d = 'A';
-  string e = 'It is a String';
+  char string[] = "It is a String";
   
     printf("%d \n",a);
     printf("%f \n",b);
-    printf("%f \n",c);
-    printf("%s \n",d);
-    printf("%c \n",e);
+    printf("%lf \n",c);
+    printf("%c \n",d);
+    printf("%s \n",string);
   return 0;
   
 }
